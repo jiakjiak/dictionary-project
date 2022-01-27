6a65e2486b9e0777fc2cbd0ca617f088dcf86c8e
jiakjiak <88105937+jiakjiak@users.noreply.github.com>
2022-01-27 16:45:16 -0700

    Added web audio api

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
   //console.log(props.phonetic);
+  let [sound, setSound] = useState(null);
 
-  return (
-    <div className="phonetic">
-      <i class="far fa-play-circle"></i> {props.phonetic.text}
-    </div>
-  );
+  function audio(event) {
+    setSound(event.props.phonetic.audio);
+    <ReactAudioPlayer src={sound} onPlay controls />;
+
+    return (
+      <div className="phonetic">
+        <i class="far fa-play-circle" onClick={audio}></i> {props.phonetic.text}
+      </div>
+    );
+  }
 }
