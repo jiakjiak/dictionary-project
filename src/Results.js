98d9bf649362d6154ecc9ad3f362ce291aa52308
jiakjiak <88105937+jiakjiak@users.noreply.github.com>
2022-02-01 20:40:37 -0700

    Styling: improved the user interface

--- src/Results.js
+++ src/Results.js
@@ -7,18 +7,23 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="results">
-        <h2>{props.results.word}</h2>
-        {props.results.phonetics.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetic phonetic={phonetic} />
-            </div>
-          );
-        })}
+        <section className="bg-light mx-5 mt-4 mb-3 p-3 text-light bg-opacity-10 rounded-3">
+          <h2>{props.results.word}</h2>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
+        </section>
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
-              <Meaning meaning={meaning} />
+              <section className="bg-light mx-5 m-3 p-2 text-light bg-opacity-10 rounded-3 fw-lighter">
+                <Meaning meaning={meaning} />
+              </section>
             </div>
           );
         })}
