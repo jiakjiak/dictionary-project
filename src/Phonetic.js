f8538a9461481c587dfd08c15962a5c77ceacf40
jiakjiak <88105937+jiakjiak@users.noreply.github.com>
2022-02-01 17:59:21 -0700

    Added and updated pronunciation audio icon display

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+import { faVolumeMute } from "@fortawesome/free-solid-svg-icons";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
@@ -11,10 +15,20 @@ export default function Phonetic(props) {
   if (props.phonetic.audio) {
     return (
       <div className="phonetic">
-        <i class="far fa-play-circle" onClick={start}></i> {props.phonetic.text}
+        <span className="phoneticAudio">
+          <FontAwesomeIcon icon={faVolumeUp} onClick={start} />
+        </span>{" "}
+        {props.phonetic.text}
       </div>
     );
   } else {
-    return <div className="phonetic">{props.phonetic.text}</div>;
+    return (
+      <div className="phonetic">
+        <span className="phoneticAudioMute">
+          <FontAwesomeIcon icon={faVolumeMute} onClick={start} />
+        </span>{" "}
+        {props.phonetic.text}
+      </div>
+    );
   }
 }
